05ff896c638bcb55c62097f5336048c8dff0d4e7
abriskin <abriskin2021@k12.andoverma.us>
2020-05-30 14:31:25 -0400

    started work on main activity

--- app/src/main/java/com/example/reccomendations/MainActivity.java
+++ app/src/main/java/com/example/reccomendations/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.reccomendations;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 //user picks recommendation type
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClicked(View view) {
+        switch(view.getId()){
+            case R.id.movie_button:
+                break;
+            case R.id.music_button:
+                break;
+        }
+
     }
 }
